c6cd5673b84b428dc80e4f454100b29a24ccc8c4
larryeitel <larry@eitel.com>
2012-12-08 09:15:10 -0600

    start for shiv models helpers

---
+++ pyfem/tests/test_models_helpers.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest  # NOQA
+
+from core import BaseMongoTestCase
+
+class GenericModelsHelpers(BaseMongoTestCase):
+    def setUp(self):
+        super(GenericModelsTests, self).setUp()
+
+    def test_prs(self):
+        uc = self.usecase
+        from models import Email, Prs
+
+        uc.load('usecases')
+        cmds = uc.run_all('one')
+        assert len(cmds) == 2
+
+
+if __name__ == "__main__":
+    unittest.main()
